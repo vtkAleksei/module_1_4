222dd7da1e3181f3fbcff3c91634ebc0e7e13b59
vtkAleksei <79051905+vtkAleksei@users.noreply.github.com>
2024-10-04 23:39:19 +0400

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('Введите произвольный текст: ')
+print('Количество символов в строке:', len(my_string))
+print('Строка в верхнем регистре:', my_string.upper())
+print('Строка в нижнем регистре:', my_string.lower())
+print('Строка без пробелов:', my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
